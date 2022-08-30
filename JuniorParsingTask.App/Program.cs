07fc18574399dc27cdb521c14b073df4d12027a9
Francuz94 <Forward552@gmail.com>
2022-08-30 11:59:09 +0200

    Dodaj pliki projektów.

---
+++ JuniorParsingTask.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using JuniorParsingTask;
+
+//tree for searching
+var tree = TreeService.Create();
+string value = "dataedo";
+
+Tree t = new Tree(new Node("root"));
+ 
+
+foreach (var item in t)
+{
+    if(item.Value == value) Console.WriteLine("Coś wyszło");
+    Console.WriteLine("test");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//SearchNode s = new SearchNode();
+//Node node = tree.Root;
+//bool resault = s.TryGetNode(value,out node);
+Console.WriteLine(value);
+
+
+
+Console.ReadKey();
---
+++ JuniorParsingTask/SearchNode.cs
@@ -0,0 +1,46 @@
+﻿
+namespace JuniorParsingTask
+{
+
+    public class SearchNode
+    {
+
+        public bool TryGetNode(string value, out Node node)
+        {
+            var tree = TreeService.Create();
+            node = tree.Root;
+
+            foreach (var item in node.Children)
+                {
+                    if (item.Value == value)
+                    {
+                        node = item;
+                        return true;
+
+                    }
+                    else
+                    {
+                        foreach (var i in item.Children)
+                        {
+                        node = item;
+                            return true;
+                        }
+                    }
+                }
+//            }
+            //else
+            //{
+                node = null;
+                System.Console.WriteLine("Tree is empty!");
+                return false;
+            //}
+
+
+
+
+        }
+
+    
+    }
+}
+
